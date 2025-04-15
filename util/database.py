f6f8cee6863e21091d5d86586535e5e619f88951
Isaiah <breaduc@gmail.com>
2025-04-15 13:08:46 -0400

    pushed db idk why it didnt b4 sry

---
+++ util/database.py
@@ -0,0 +1,18 @@
+import json
+import sys
+import os
+from pymongo import MongoClient
+
+docker_db = os.environ.get('DOCKER_DB', "false")
+
+if docker_db == "true":
+    print("using docker compose db")
+    mongo_client = MongoClient("mongo")
+else:
+    print("using local db")
+    mongo_client = MongoClient("localhost")
+
+db = mongo_client["312mmo"]
+
+user_collection = db["users"]
+# prob need leaderboards and other thing stoed, just add as you go
